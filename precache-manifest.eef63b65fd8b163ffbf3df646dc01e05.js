4277f0c7a47f1f5eb4ae6ea119416753670e4ee9
changxinaglin <281532612@qq.com>
2020-11-12 16:25:03 +0800

    Updates

---
+++ precache-manifest.eef63b65fd8b163ffbf3df646dc01e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd64e4e12066da4f170813a0e9a99fc7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d7b628e32364c61fa40",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "363a4377d55622187d8b",
+    "url": "./static/js/2.5960c641.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5960c641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7b628e32364c61fa40",
+    "url": "./static/js/main.ed75275a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.ed75275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216a6fb528a8db640590",
+    "url": "./static/js/runtime-main.551fa229.js"
+  }
+]);
