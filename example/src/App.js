64b50ae8a88395c3a2b4d78ae0de9bff139013b2
changxinaglin <281532612@qq.com>
2020-11-12 15:09:29 +0800

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+// import React from 'react'
 
-import { ExampleComponent } from 'reactlinui'
-import 'reactlinui/dist/index.css'
+// import { ExampleComponent } from 'reactlinui'
+// import 'reactlinui/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+// const App = () => {
+//   return <ExampleComponent text="Create React Library Example 😄" />
+// }
+
+// export default App
+
+
+import React, { Component } from 'react'
+
+import { ColoredHeadingOne, ColoredHeadingTwo } from 'reactlinui'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ColoredHeadingOne
+          text="Colored heading one is working"
+          color={'red'}
+        />
+
+        <ColoredHeadingTwo
+          text="Colored heading one is working"
+          color={'blue'}
+        />
+      </div>
+    )
+  }
 }
-
-export default App
